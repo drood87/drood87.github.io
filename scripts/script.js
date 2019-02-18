3b74a66da544b7e4fb30305ef433130044151a3e
drood <dbernstein1987@hotmail.com>
2019-02-18 20:20:09 +1100

    js hooks and testing

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
-const peopleCard = document.querySelector("#people");
-const planetCard = document.querySelector("#planet");
+const peopleCard = document.querySelector('#people');
+const planetCard = document.querySelector('#planets');
+const speciesCard = document.querySelector('#species');
+
+function sayHi() {
+  console.log('Hi 👍');
+}
+
+peopleCard.addEventListener('click', sayHi);
+planetCard.addEventListener('click', sayHi);
+speciesCard.addEventListener('click', sayHi);

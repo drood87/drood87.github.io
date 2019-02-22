bf709223a3cc6a1f404a6c73e55ef3eacb219036
drood <dbernstein1987@hotmail.com>
2019-02-22 19:07:47 +1100

    refactor new functions with async/await

--- scripts/script.js
+++ scripts/script.js
@@ -37,10 +37,30 @@ function filterData(data) {
   });
 }
 
+//check if array has a method with another page
+
 function nextPageCheck(pageCheck) {
   if (pageCheck != null) {
     nextPageData(pageCheck);
   }
+}
+
+async function peopleData() {
+  const peoplePromise = await fetch(peopleApiPoint).then(r => r.json());
+
+  peoplePromise.results.map(res => {
+    const { name, homeworld, birth_year } = res;
+
+    async function getPlanet() {
+      const planetPromise = await fetch(homeworld).then(r => r.json());
+      const planetName = planetPromise.name;
+      console.log(planetName);
+      return planetName;
+    }
+
+  });
+  const finalData = await Promise.all([peoplePromise, getPlanet()]);
+  console.log(finalData);
 }
 
 function createTextContent(name, population, diameter) {
@@ -56,6 +76,6 @@ function createTextContent(name, population, diameter) {
   sectionDiv.appendChild(div);
 }
 
-// peopleCard.addEventListener('click', getPeopleData);
+peopleCard.addEventListener('click', peopleData);
 planetCard.addEventListener('click', planets);
 // speciesCard.addEventListener('click', getSpeciesData);
